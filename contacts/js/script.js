fe334603655b8e6301e96e0bea4e2d5a3e51e865
Seif Wael <104922495+Psycho-seif@users.noreply.github.com>
2023-01-21 04:28:08 +0200

    Add files via upload

---
+++ contacts/js/script.js
@@ -0,0 +1,18 @@
+var typed = new Typed(".typing",{
+    strings:["","Ahmed Samir"],
+    typeSpeed:90,
+    BackSpeed:40,
+    loop:true
+})
+var typed = new Typed(".typing1",{
+    strings:["","Youssif Mohamed"],
+    typeSpeed:90,
+    BackSpeed:40,
+    loop:true
+})
+var typed = new Typed(".typing2",{
+    strings:["","Mohannad Mohamed"],
+    typeSpeed:90,
+    BackSpeed:40,
+    loop:true
+})
